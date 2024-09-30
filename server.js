462b08ad05718e526ee6663a4b6aaa95b331630d
Scott B Bowman <145110529+Arcarius41@users.noreply.github.com>
2024-09-30 21:52:23 +0000

    Simplified CSP and verified asset paths

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 4000; //port number here
 // Add Content Security Policy (CSP) headers
 app.use((req, res, next) => {
   res.setHeader(
-    "Content-Security-Policy", 
-    "default-src 'self'; img-src 'self' data: https:; script-src 'self'; style-src 'self' 'unsafe-inline';"
+    "Content-Security-Policy",
+    "default-src *; img-src * data:; script-src * 'unsafe-inline'; style-src * 'unsafe-inline';"
   );
-  
   next();
 });
+
 
 
 
