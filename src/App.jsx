3d3e720e4225b469d438034740b5459767cb05a8
Scott B Bowman <145110529+Arcarius41@users.noreply.github.com>
2026-02-23 02:37:05 +0000

    Changed out images Feb 2026

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
